89047a594d39368f50d9c27e8ed523b37c1dc3ce
zhanghaodong <zhanghaodong06524@hellobike.com>
2020-01-26 17:10:13 +0800

    图片静态资源

--- src/main/java/com/zhd/ultimate/sociology/interceptor/WebConfig.java
+++ src/main/java/com/zhd/ultimate/sociology/interceptor/WebConfig.java
@@ -2,7 +2,7 @@ package com.zhd.ultimate.sociology.interceptor;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * @author: zhanghaodong
@@ -10,11 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @date: 2020-01-26 16:57
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/templates/");
         //映射图片保存地址
         registry.addResourceHandler("/image/**").addResourceLocations("file:/Users/zhanghaodong/work/myproject/image/");
+        super.addResourceHandlers(registry);
     }
 }
